f6a2f4d04caff4269344315354fcf1f5a427bb68
Asapanna Rakesh <rakeshark22@gmail.com>
2020-07-05 15:59:42 +0530

    divisors done

--- 00-HackerEarth/01-Basic-Programmin/04-soln.py
+++ 00-HackerEarth/01-Basic-Programmin/04-soln.py
@@ -7,7 +7,7 @@ class Solution:
     def solve(self):
         cntr = 0 
         for i in range(l, r+1):
-            if self.k%1 == 0: cntr += 1
+            if i%self.k == 0: cntr += 1
         return cntr    
 
 

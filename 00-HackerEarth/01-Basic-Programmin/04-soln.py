85e562652043a85ad90dfd6964ce065df2f49f59
Asapanna Rakesh <rakeshark22@gmail.com>
2020-07-05 15:54:40 +0530

    divisors done

---
+++ 00-HackerEarth/01-Basic-Programmin/04-soln.py
@@ -0,0 +1,17 @@
+class Solution:
+    def __init__(self, l, r, k):
+        self.l = l
+        self.r = r
+        self.k = k
+
+    def solve(self):
+        cntr = 0 
+        for i in range(l, r+1):
+            if self.k%1 == 0: cntr += 1
+        return cntr    
+
+
+if __name__ == '__main__':
+    l, r, k = list(map(int,input().split()))
+    soln = Solution(l,r,k)
+    print(soln.solve())

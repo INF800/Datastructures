1389f6e78edfbcd8709376b70aec70a701c224b4
Asapanna Rakesh <rakeshark22@gmail.com>
2020-07-08 18:49:28 +0530

    new commit

--- 00-HackerEarth/01-Basic-Programmin/Implementation/01-sub-matrices.py
+++ 00-HackerEarth/01-Basic-Programmin/Implementation/01-sub-matrices.py
@@ -5,16 +5,44 @@ class numSubMatricesSumDivisibleByP:
         self.P      = P
         self.matrix = matrix
 
-    def brute(self):
-        sum = 0
-        cntr = 0
+    def brute0(self):
+        '''
+        TIME        : O(n^4)
+        '''
+        n_sub_mat_sum_div_by_p = 0
+
+        for r in range(0, self.rows):
+            for c in range(0, self.cols): 
+                for br in range(r, self.rows): # row cur
+                    for bc in range(c, self.cols): # col cursor
+
+                        # total (m)(m+1)(n)(n+1)/ 4 submatrices = 36 sub matrices for 3x3
+                        #print(f'top idx:({r}, {c}) \t  bottom idx:({br}, {bc})')
+                        sum = 0
+                        for row in range(r, br+1):
+                            for col in range(c, bc+1):
+                                sum += self.matrix[row][col]
+                        #print('sum: ', sum) 
+                        if sum%self.P == 0:
+                            n_sub_mat_sum_div_by_p += 1
         
-        # O(n^4)
-        
-        return cntr
+        return n_sub_mat_sum_div_by_p
+
+    
+    def eff(self):
+        '''
+        TIME        : O(n^3)
+        '''
+        pass
 
 
 if __name__ == '__main__':
+    '''
+    3 3 99
+    1 2 3
+    4 5 6
+    7 8 9
+    '''
     matrix = []
     M, N, P = list(map(int,input().split()))
     for _ in range(0, M):
@@ -22,5 +50,5 @@ if __name__ == '__main__':
         matrix.append(row)
 
     soln = numSubMatricesSumDivisibleByP(M, N, P, matrix)
-    print(soln.brute())
     
+    print(soln.brute0())

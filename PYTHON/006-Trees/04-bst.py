adca3b02e2d73af9a2cf42e76da2a87e4d10ccdc
Asapanna Rakesh <rakeshark22@gmail.com>
2020-07-16 17:44:39 +0530

    updated

--- PYTHON/006-Trees/04-bst.py
+++ PYTHON/006-Trees/04-bst.py
@@ -1,9 +1,3 @@
-"""
-BST
-    - 
-"""
-
-
 class Node:
     """ Node (of BST)
     DATA MEMBERS  
@@ -81,17 +75,22 @@ class BST:
 
     METHODS
         - insert            : (build tree) appends ele to tree using @staticmethod __insert(node, ele)
+        - insert_unique     : (build tree) same as above but only unique values (CAN BE USED FOR INDEXED SEARCH)
         - __iter__          : (traverse tree) exploit mutually recursive iter in Node
                                     + Simply returns mutually recursive iterator() of root node
-
         - __contains__      : exploit iterator
-        - __del__           : exploit iterator
+        - __del__           : key is to get prev node
     """
-    def __init__(self, contents=[]):
+    def __init__(self, contents=[], unique=False):
         self.root = None
+        
+        self.is_unique = True if unique is True else False
 
         for e in contents:
-            self.insert(e)
+            if unique == False:
+                self.insert(e)
+            elif unique==True:
+                self.insert_unique(e)
 
     def insert(self, ele):
         """ Accepts duplicates """
@@ -106,7 +105,34 @@ class BST:
             # else,
             if e < node.get_item():
                 node.set_left( __insert(node.get_left(), e) )
-            else: # accept duplicates too
+            else: # accept duplicates too by creating new node for them on right
+                node.set_right( __insert(node.get_right(), e) )
+
+            return node # return 2of2
+        # end def __insert
+
+        # append / insert to root
+        self.root = __insert(self.root, ele)
+
+    def insert_unique(self, ele):
+        """ Accepts duplicates """
+
+        def __insert(node, e):
+            """
+            - if node is empty, append new node w/ e
+            - if not empty, recursive call (in-order asc.)
+            """
+            if node == None: return Node(item=e) # return 1of2
+
+            # else,
+            if e < node.get_item():
+                node.set_left( __insert(node.get_left(), e) )
+            
+            # detect dups if wanted
+            # if node.get_item() == e: pass # do nothing, or log dups
+            if node.get_item() == e: print('duplicate: ', e)
+
+            if e > node.get_item(): # accept duplicates too
                 node.set_right( __insert(node.get_right(), e) )
 
             return node # return 2of2
@@ -125,10 +151,28 @@ class BST:
 # --------------------------------------------------------------------------------------------------------
 
 if __name__ == '__main__':
+    def print_line(head='',s='+', x=100): 
+        print('\n')
+        print(s*x)
+        print('+ '+head)
+        print(s*x)
      
-    lst    = [1,4,-3,5,0] # only ordinal data. str not allowed
-    bst    = BST(lst)
+    lst = [1,0,4,-3,1,5,0] # only ordinal data. str not allowed. 
 
-    # 1. iterator
+    # 1. test insert()
+    print_line('1. test insert()')
+    bst = BST(lst)
+
+    # 2. test iterator
+    print_line('2. test iterator')
     for e in bst:
-        print(e) 
+        print(e, end=' ') 
+
+    # 3. test unique bst
+    print_line('3. test unique bst')
+    bst2 = BST(lst, unique=True)
+    for e in bst2:
+        print(e, end=' ') 
+
+    # 4. delete
+    pass

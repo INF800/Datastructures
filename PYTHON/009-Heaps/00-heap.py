260e32572a7eccbe3a76fd6fe419ec35f1f8c9bc
Asapanna Rajesh <rajesh.asapanna@gmail.com>
2020-08-16 01:31:31 +0530

    todo: siftup, heapify, delete

--- PYTHON/009-Heaps/00-heap.py
+++ PYTHON/009-Heaps/00-heap.py
@@ -7,6 +7,12 @@
 
 class Heap:
     """
+    KEY: Indices start from 1 not 0.
+        + parent_idx        = child_idx//2
+        + left_child_idx    = parent_idx*2
+        + right_child_idx   = parent_idx*2 + 1 
+
+
     DATA MEMBERS
         + _list                     : internal list representation of heap
         + __internal_list_size      : actual size of internal list
@@ -18,15 +24,18 @@ class Heap:
                                         -  O(nlogn)
                                         - `__heapify` [ O(n) ammortized] is better than `__sift_up`
 
-    HELPER METHODS 
+    HELPER METHODS
+        + __get_cur                 : returns to the last position idx (>=1 cz inices start from 1) in _list of heap
+        + __append                  : appends to last empty pos of _list making sure indices start from 1
         + __make_room(max_ratio)    : if `_list` is full more than `max_ratio` increases size
         + __sift_up                 : 
         + __hepify                  : 
     """
 
     def __init__(self, contents=[], initial_size=10):
-        self._list = [None] # idxs start from 1 not 0
-        self._size = 0
+        # Note: idxs start from 1 not 0
+        self._list  = [None]*initial_size 
+        self._size  = 0
 
         self.__internal_list_size = initial_size
 
@@ -35,51 +44,70 @@ class Heap:
     
     # ----------------------------------------------------------
     # Insertion
-    # ----------------------------------------------------------
-    def __make_room(self, max_ratio=0.75):
-        cur_ratio = self.__internal_list_size / self._size
+    # ----------------------------------------------------------t
+    def __get_cur(self):
+        return  self._size + 1
+
+    def __append(self, e):
+        """
+        makes sure indices start from 0
+
+        Note: does not append to `Heap`. Appends to `_list`
+        """
+        self._list[self.__get_cur()] = e
+        self._size += 1
+        # no need to increment self.cur by 1 because,
+        # it is dependant on size. (_size + 1)
+
+    def __make_room(self, max_ratio=0.75, factor=2):
+        # No need to make room at beginig of internal list 
+        # as it will never be empty.
+        cur_ratio = self._size / self.__internal_list_size
         if cur_ratio > max_ratio:
-            new_list = [None] * (2 * self.__internal_list_size)
+            new_list = [None] * (factor * self.__internal_list_size)
             for idx, e in enumerate(self._list):
                 new_list[idx] = e
+            # update:
             # self._size remains same
             self._list = new_list 
+            self.__internal_list_size = factor * self.__internal_list_size
 
-    def __siftUp(self, cur_idx, cur_e):
+    def __siftUp(self, e):
         """
         TIME:   O(logn)
         """
         
-        parent_idx  = cur_idx // 2
-        parent_ele  = self._list[parent_idx]
-        temp        = cur_e
-
-        while (cur_idx>1) or (cur_e < parent_ele):
-            # make parent child
-            self._list[cur_idx] = self._list[parent_idx]
-            
-            cur_idx = cur_idx // 2
+        # write sift up code here
+        self.__append(e)
+        print(self._list)
 
 
-
-    def insert_from(self, a_sequence):
+    def insert_from(self, a_sequence, method="siftup"):
         """
-        heapify is more efficient -- O(n)
+        heapify is more efficient       --> O(n) ammortized
+        Here, we are using __siftUp     --> O(nlogn)
 
         TIME        : O(nlogn)
         """
+        # Note: idxs start from 1 not 0
         for e in a_sequence:
-            
-            # initial condition (executed only once)
-            # directly add root as there won't be anything to compare
-            if self._list == [None]:
-                self._list.append(e)
-                self._size = 1
-                continue
 
-            # makeroom if filling > 0.75 ratio
-            self.__make_room(0.75)
+                # initial condition (executed only once)
+                # directly add root as there won't be anything to compare
+                if self._size == 0:
+                    self.__append(e)
+                    continue
 
-            # add element to heap
-            last_idx = self._size + 1
-            self.__siftUp(last_idx, e)
+                # makeroom if load > 0.75 ratio
+                self.__make_room(0.75)
+
+                # add element to heap using heapify/siftup
+                if   method == "siftup"  : self.__siftUp(e)
+                elif method == "heapify" : self.__heapify(e)   
+
+                
+
+if __name__ == '__main__':
+    
+    lst = [11,77,33,66,99,33,66,44,-44,-22,-33]
+    h = Heap(lst)
